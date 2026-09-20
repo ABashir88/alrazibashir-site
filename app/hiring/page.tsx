e79e47ddfa0b71c9b6cf653a251ec275296edf78
Alrazi Bashir <alrazibashir@Alrazis-MacBook-Pro.local>
2026-09-20 03:42:33 -0400

    fix: correct config import path

--- app/hiring/page.tsx
+++ app/hiring/page.tsx
@@ -1,6 +1,6 @@
 "use client";
 import Link from "next/link";
-import { siteConfig } from "../config";
+import { siteConfig } from "../src/config";
 
 export default function HiringPage() {
   return (
