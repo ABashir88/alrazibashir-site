f16de3db6c5f8f95211e88e34eb7271acb03c86d
Alrazi Bashir <alrazibashir@Alrazis-MacBook-Pro.local>
2026-09-20 03:02:01 -0400

    fix: named import for siteConfig

--- src/app/hiring/page.tsx
+++ src/app/hiring/page.tsx
@@ -1,7 +1,7 @@
 "use client";
 
 import Link from "next/link";
-import siteConfig from "../../config";
+import { siteConfig } from "../../config";
 
 export default function HiringPage() {
   return (
